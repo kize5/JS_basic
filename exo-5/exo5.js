f4c59988fe98a849bed827d625f4de17492d51ff
kize5 <bochu.nicolas@hotmail.fr>
2023-01-11 15:21:45 +0100

    exo5 fini mais sans indexOf

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,20 @@
 // you can write js here
 
 console.log('exo-5');
+
+const input = "Coder c'est rigolo en JavaScript";
+const vowels = "aeiouy"; /*str agis comme un tableau*/
+resultArray = [];
+
+/*Revenir pour utiliser la method indexOf() */
+for (let i=0; i < input.length; i++) {
+    for (let x=0; x < vowels.length; x++){
+        if (input[i] === vowels[x]) {
+            resultArray.push(input[i])
+            console.log(input[i])
+        }
+    }
+}
+resultArray.join();
+// resultArray.toUpperCase();
+console.log(resultArray.join(" ").toUpperCase())
