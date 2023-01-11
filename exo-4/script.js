8f3341bbe5f532e91feda4ea6173162f51a8271a
kize5 <bochu.nicolas@hotmail.fr>
2023-01-11 14:31:02 +0100

    exo4 fini

--- exo-4/script.js
+++ exo-4/script.js
@@ -2,4 +2,22 @@
 
 console.log('exo-4');
 
-var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+let secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily",
+    "the", "first", "time,", "it's", "about", "what", "you", "can", "figure",
+    "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+/*Retier le dernier élément du tableau secretMessage*/
+// function popIt (x) {
+//     let oneLess = x.pop();
+//     return oneLess;
+// }
+// popIt(secretMessage);
+
+secretMessage.pop();
+secretMessage.push('to', 'program');
+secretMessage[6] = "right";
+secretMessage.shift();
+secretMessage.unshift("Programming");
+secretMessage.splice(5, 5, "know");
+phrase = secretMessage.join(' ');
+console.log(phrase); /*Test function popIt*/
