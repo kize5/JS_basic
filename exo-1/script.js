7ca0d83b9280008c824985c122a4ae03c0c5115b
kize5 <bochu.nicolas@hotmail.fr>
2023-01-11 10:21:20 +0100

    exo 1 fini

--- exo-1/script.js
+++ exo-1/script.js
@@ -1,2 +1,12 @@
 // you can write js here
 console.log('hello from file');
+
+let kelvins = prompt("Quelle est la température en Kelvins aujourd'hui ?");
+/*Je demande à l'utilisateur de via le prompt déclare la variable kelvins*/
+let celsuis = kelvins - 273
+let fahrenheits = celsuis * (9/5) + 32 /*J'utilise la variable celsuis + un calcul pour déclarer ma variable fahren*/
+
+fahrenheits = Math.floor(fahrenheits) /*J'arrondis fahrenheits*/
+console.log("La températeure est de " + fahrenheits + " degrés fahrenheits") /*Console log pour vérifier le résultat*/
+
+
